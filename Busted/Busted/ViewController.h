527ae97d129c5322a78b8132a87c82e76e93d909
aeisses <aaron.eisses@teamspace.ca>
2013-09-13 20:51:17 -0300

    This is the initial project commit

---
+++ Busted/Busted/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Busted
+//
+//  Created by Aaron Eisses on 2013-09-13.
+//  Copyright (c) 2013 Aaron Eisses. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Busted/Busted/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Busted
+//
+//  Created by Aaron Eisses on 2013-09-13.
+//  Copyright (c) 2013 Aaron Eisses. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
